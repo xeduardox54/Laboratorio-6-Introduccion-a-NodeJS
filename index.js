0e2c16bec26d0cb2d0bc33199253067f6d2612b2
Eduardo <xeduardox54@hotmail.com>
2021-04-24 17:51:20 -0500

    7. Convierte los require de formato commonJS a EcmaScriptModules

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-const { response } = require('express');
-const express = require('express');
+import express from 'express';
+import persons from './persons.js';
 const app = express();
 
 app.use(express.json());
 
-const persons = [
-    {
-        id:1,
-        name: "Arto Hellias",
-        number: "040-123456",
-    },
-    {
-        id:2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id:3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id:4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122",
-    },
-];
 let now= new Date();
 
 app.get('/info',(req,res) => {
-    res.send(`<h3>Phonebook has info for ${agenda.length} people</h3><h3>`+now+"</h3>");
+    res.send(`<h3>Phonebook has info for ${persons.length} people</h3><h3>`+now+"</h3>");
 });
 app.get('/api/persons',(req,res) => {
     res.send(persons);
